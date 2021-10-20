db41986c535a1b1edd3984132fd45515f011a161
Abhinavpandey9 <85674999+Abhinavpandey9@users.noreply.github.com>
2021-10-20 09:23:08 +0530

    Add files via upload

---
+++ gambler.java
@@ -0,0 +1,12 @@
+public class Gambler{
+    void UC1{
+    int stake = 100;
+    int bet = 1;
+    System.out.println("Stake started per day $" + stake);
+    System.out.println("bet in each game $" + bet);
+    }    
+      public static void main(String args{}){
+      System.out.println("Start the Gambler Simulator"); 
+      Gambler gambler = new Gambler();
+      }
+
